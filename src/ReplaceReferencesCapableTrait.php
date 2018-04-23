dad35a6f2526ff4eb4edc9131359c75fa4c701d6
Roman Lytvynenko <litvinenko95@gmail.com>
2018-04-23 12:24:02 +0300

    fixed docs and added preg_quote delimiter

--- src/ReplaceReferencesCapableTrait.php
+++ src/ReplaceReferencesCapableTrait.php
@@ -11,25 +11,24 @@ use Psr\Container\NotFoundExceptionInterface;
 /**
  * Trait ReplaceReferencesCapableTrait.
  *
- * Adds ability to replace all tokens in the form `${key}` in a string with corresponding values retrieved from a container.
+ * Adds ability to replace all tokens wrapped with some delimiters in a string with corresponding values retrieved from a container.
  *
  * @since [*next-version*]
  */
 trait ReplaceReferencesCapableTrait
 {
     /**
-     * Replaces all tokens in the form `${key}` in a string with corresponding values retrieved from a container.
+     * Replaces all tokens wrapped with some delimiters in a string with corresponding values retrieved from a container.
      *
      * @since [*next-version*]
      *
      * @param string|Stringable  $input          Input string to find and replace references
-     * @param ContainerInterface $container
-     * @param string|Stringable  $startDelimiter
+     * @param ContainerInterface $container      Container to retrieve values for replace.
+     * @param string|Stringable  $startDelimiter Starting delimiter of token for replace
      * @param mixed              $default        String to replace reference if key not found in container.
-     * @param string|Stringable  $endDelimiter
+     * @param string|Stringable  $endDelimiter   Ending delimiter of token for replace
      *
-     * @throws ContainerExceptionInterface
-     * @throws NotFoundExceptionInterface
+     * @throws ContainerExceptionInterface Error while retrieving the entry from the container.
      *
      * @return string The resulting string.
      */
@@ -38,8 +37,8 @@ trait ReplaceReferencesCapableTrait
         $input        = $this->_normalizeString($input);
         $defaultValue = $default === null ? '' : $this->_normalizeString($default);
 
-        $startDelimiter = preg_quote($this->_normalizeString($startDelimiter));
-        $endDelimiter   = preg_quote($this->_normalizeString($endDelimiter));
+        $startDelimiter = preg_quote($this->_normalizeString($startDelimiter), '/');
+        $endDelimiter   = preg_quote($this->_normalizeString($endDelimiter), '/');
 
         $regexp = '/' . $startDelimiter . '(.*?)' . $endDelimiter . '/';
 
